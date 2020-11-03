9fd87f88cc7280a86d00e4a859bce46a4d031065
Suyash <suyashpathak143@gmail.com>
2020-11-03 08:47:17 +0530

    added User Profile Page, Change Password Functionality and fixed registration password bug

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [path('register', views.register, name='register'),
                path('login',views.login,name='login'),
-               path('logout',views.logout,name='logout')
+               path('logout',views.logout,name='logout'),
+               path('profile',views.profile,name='profile'),
+               path('change_password/',views.change_password, name='change_password'),
                ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
 from store.models import Customer
 from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
 
 
 def register(request):
@@ -12,30 +14,33 @@ def register(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
         email = request.POST['email']
-        if password1==password2:
-            if User.objects.filter(username=username):
-                messages.info(request,'Username Taken')
-                return redirect('register')
-
-            elif User.objects.filter(email=email):
-                messages.info(request,'Email Taken')
-                return redirect('register')
-            else:
-                user = User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
-                user.save()
-                print('user created')
-                name = first_name+' '+last_name
-                customer,created = Customer.objects.get_or_create(user=user,name=name,email=email,)
-                customer.save()
-
-                return redirect('login')
-
-        else:
-            messages.info(request,'passoword incorrect')
+        if len(password1)<8:
+            messages.info(request,"Password Should be Atleast 8 character long")
             return redirect('register')
+        else:
+            if password1==password2:
+                if User.objects.filter(username=username):
+                    messages.info(request,'Username Taken')
+                    return redirect('register')
+
+                elif User.objects.filter(email=email):
+                    messages.info(request,'Email Taken')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
+                    user.save()
+                    print('user created')
+                    name = first_name+' '+last_name
+                    customer,created = Customer.objects.get_or_create(user=user,name=name,email=email,)
+                    customer.save()
+
+                    return redirect('login')
+
+            else:
+                messages.info(request,'passoword incorrect')
+                return redirect('register')
     else:
         return render(request,'register.html')
-
 
 
 def login(request):
@@ -53,7 +58,28 @@ def login(request):
     else:
         return render(request,'login.html')
 
+
 def logout(request):
     auth.logout(request)
     return redirect('/')
 
+
+def profile(request):
+    return render(request,'profile.html')
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'change_password.html', {
+        'form': form
+    })
